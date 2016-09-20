b321d89b2fd82c511d492860b1996a26caba8bda
Andrew MacPherson <andrew@paterva.com>
2016-09-21 00:07:31 +0200

    WiFi via EEPROM, Initial Make Request

--- BSidesBadge2016/BSidesBadge2016.ino
+++ BSidesBadge2016/BSidesBadge2016.ino
@@ -1,18 +1,52 @@
+/* ESP8266 WiFi */
 #include "ESP8266WiFi.h"
 #include <ESP8266mDNS.h>
 #include <WiFiClient.h>
+#include <ESP8266HTTPClient.h>
+
+/* EEPROM */
 #include <EEPROM.h>
 #include "EEPROMAnything.h"
 
+/* LCD */
+#include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
+#include "OLEDDisplayUi.h" // Include the UI lib
+SSD1306  display(0x3c, 5, 2);
+OLEDDisplayUi ui     ( &display );
+
+/* Timer */
+#include "Timer.h"
+Timer t;                               //instantiate the timer object
+
+/*Infrared */
+#include <IRremoteESP8266.h>
+const int TX_PIN = 10;
+IRsend irsend(TX_PIN); //an IR led is connected to GPIO pin 0
+
+int RECV_PIN = 4; 
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+
+/* Shift Out ( 74HC595 ) */
+const int latchPin = 12;
+const int clockPin = 14;
+const int dataPin = 13;
+
+/* Shift In  */
+const int pinShcp = 15; //Clock
+const int pinStcp = 0; //Latch
+const int pinDataIn = 16; // Data
+
 MDNSResponder mdns;
 WiFiServer server(80);
+HTTPClient http;
+
 boolean debug = true;
 
 char defaultSSID[32]     = "SSID";
 char defaultPassword[32] = "Password";
 
-char* currentSSID = "";
-char* currentPassword = "";
 
 struct WiFiSettings {
   char ssid[32];
@@ -20,7 +54,11 @@ struct WiFiSettings {
 } currentWiFi;
 
 
-
+uint8_t MAC_array[6];
+char MAC_char[18];
+/*
+ * Connects to Wifi Network and tries for <attempts> seconds
+ */
 boolean wifiConnect(char* wSSID,char* wPassword,int attempts)
 {
   Serial.print("[+] Connecting to network:");Serial.print(wSSID);Serial.print("-- password:");Serial.print(wPassword);Serial.println(".");
@@ -37,23 +75,32 @@ boolean wifiConnect(char* wSSID,char* wPassword,int attempts)
     }
   }
   Serial.println("PASSED.");
+ 
+
+  WiFi.macAddress(MAC_array);
+  
+  Serial.print("MAC Address:");
+  for (int i = 0; i < sizeof(MAC_array); ++i){
+    sprintf(MAC_char,"%s%02x:",MAC_char,MAC_array[i]);
+  }
+
+  Serial.println(MAC_char);
+
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  
   return true;
 }
 
+/*
+ * Initialise Wifi
+ * ---------------
+ * This first tries the default creds for 30s, then looks to see 
+ * if anything is stored in the EEPROM and then asks the user for SSID/PW
+ */
 
-void setup() {
-  //Setup Serial Connection at ESP debug speed ( so we get debug information as well )
-  Serial.begin(74880);
-  
-  //Setup EEPROM - 512!
-  EEPROM.begin(512);
-  
-  if(debug == true)
-  {
-    Serial.println("Starting Badge...");
-    Serial.println("[+] Connecting to default WiFi");
-  }
-
+void initWiFi()
+{
   // Try connect to the default WiFi SSID and Password
   boolean defaultConnect;
   defaultConnect = wifiConnect(defaultSSID,defaultPassword,1);
@@ -61,8 +108,6 @@ void setup() {
   
   if(defaultConnect == true)
   {
-    
-    
     strcpy(currentWiFi.ssid, defaultSSID);
     strcpy(currentWiFi.password, defaultPassword);
   }
@@ -70,24 +115,19 @@ void setup() {
   {
     // Default SSID/PW doesnt work, lets try from the EEPROM
     char* EEPROMSSID;
-    char* EEPROMPassword;
-
-    
+    char* EEPROMPassword;   
     boolean EEPROMConnect = false;
     
     EEPROM_readAnything(0, currentWiFi);
-    
-    
     EEPROMConnect = wifiConnect(currentWiFi.ssid,currentWiFi.password,10);
     
 
     if(EEPROMConnect == true)
     {
-      //currentSSID = EEPROMSSID;
-      //currentPassword = EEPROMPassword;
-      strcpy(currentWiFi.ssid, EEPROMSSID);
-      strcpy(currentWiFi.password, EEPROMPassword);
-      
+      //Serial.println("Connected1!");
+      //strcpy(currentWiFi.ssid, EEPROMSSID);
+      //strcpy(currentWiFi.password, EEPROMPassword); 
+      //Serial.println("Connected2!");
     }
     else
     {
@@ -95,7 +135,6 @@ void setup() {
       
       char SerialSSID[32] = "";
       char SerialPassword[32] = "";
-      
       String str;
 
       Serial.println("Please enter SSID:");
@@ -123,8 +162,6 @@ void setup() {
 
       if(SerialConnect == true)
       {
-        //currentSSID = SerialSSID;
-        //currentPassword = SerialPassword;
         strcpy(currentWiFi.ssid, SerialSSID);
         strcpy(currentWiFi.password, SerialPassword);
         
@@ -134,9 +171,8 @@ void setup() {
       }
       else
       {
-        Serial.println("Failed. Please reset to try again. Deepsleep in 5 seconds...");
+        Serial.println("Failed. Please reset to try again. ");
         delay(5000);
-        //ESP.deepsleep();
       }
       
       
@@ -144,6 +180,54 @@ void setup() {
 
     
   }
+}
+
+String makeHTTPRequest(String URL)
+{
+  if (WiFi.status() != WL_CONNECTED) 
+  {
+      http.begin(URL); 
+      // start connection and send HTTP header
+      int httpCode = http.GET();
+      if(httpCode > 0) 
+      {
+          // HTTP header has been send and Server response header has been handled
+          Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+    
+          // file found at server
+          if(httpCode == HTTP_CODE_OK) 
+          {
+              String payload = http.getString();
+              Serial.println(payload);
+          }
+      } 
+      else 
+      {
+          Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      }
+      http.end();
+  }
+}
+
+/*
+ * Main Setup for badge.
+ */
+void setup() {
+  //Setup Serial Connection at ESP debug speed ( so we get debug information as well )
+  Serial.begin(74880);
+  
+  //Setup EEPROM - 512 bytes!
+  EEPROM.begin(512);
+  
+  if(debug == true)
+  {
+    Serial.println("Starting Badge...");
+    Serial.println("[+] Connecting to default WiFi");
+  }
+
+  initWiFi(); // Initialise WiFi
+
+  makeHTTPRequest("http://badges2016.andrewmohawk.com/");
     
   
 
